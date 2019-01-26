035bfac503d7cf39693934ffde61e875ec06a692
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2019-01-26 17:17:11 -0500

    Player state + Animate (#8)

--- client/strengthbros-game-1/src/components/Player.jsx
+++ client/strengthbros-game-1/src/components/Player.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { ReactComponent as StandingSVG } from "../assets/standing.svg";
+import { ReactComponent as SquattingSVG } from "../assets/squatting.svg";
 
 class Player extends Component {
+  state = {
+    standing: true
+  };
+
   handleKeyDown = e => {
     switch (e.keyCode) {
       case 32:
         console.log("Space bar pressed");
+        this.setState(() => ({
+          standing: false
+        }));
         break;
-      case 16:
-        console.log("Shift pressed");
+      default:
+        return;
+    }
+  };
+
+  handleKeyUp = e => {
+    switch (e.keyCode) {
+      case 32:
+        console.log("Space bar unpressed");
+        this.setState(() => ({
+          standing: true
+        }));
         break;
       default:
         return;
@@ -15,11 +34,17 @@ class Player extends Component {
   };
 
   render() {
-    return <circle cx={0} cy={0} r={50} />;
+    console.log(this.state.standing);
+    if (this.state.standing) {
+      return <StandingSVG y={-50} />;
+    } else {
+      return <SquattingSVG y={-50} />;
+    }
   }
 
   componentDidMount() {
     window.onkeydown = this.handleKeyDown;
+    window.onkeyup = this.handleKeyUp;
   }
 }
 
