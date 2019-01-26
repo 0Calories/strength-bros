23c606c419d50ca64ae4bd2e2c033863ce9ceb9b
Eric Newcombe <eric@ericnewcombe.com>
2019-01-26 15:05:49 -0500

    Fixed issues with sockets, have it set up to have multiple socket files

--- server/sockets/test_socket2.js
+++ server/sockets/test_socket2.js
@@ -2,8 +2,8 @@ module.exports = function(io) {
 
     io.on('connect', function (socket) {
         
-        socket.on('event1', () => {
-            console.log(`Event 1`);
+        socket.on('event2', () => {
+            console.log(`Event 2`);
         })
 
     });
