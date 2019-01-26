bedf3f1a10f94fa06dbca4dfbc7fb1178e02ab01
0Calories <cppanand@gmail.com>
2019-01-26 13:35:28 -0500

    Add session ID generation on POST to /create endpoint

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const { generateId } = require('./utils/utils');
+
 const app = express();
 const port = 6969;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/create', (req, res) => {
+  console.log('New room created!');
+  const sessionId = generateId();
+  res.send(`Successfully created room with ID ${sessionId}`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ server/utils/utils.js
@@ -0,0 +1,3 @@
+const generateId = () => '_' + Math.random().toString(36).substr(2, 9);
+
+module.exports = { generateId };
