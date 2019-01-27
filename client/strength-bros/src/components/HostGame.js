8a630de2ac0096489d86278d68e9428ade0041f1
tomalama <tommy.nguyen7@hotmail.com>
2019-01-27 07:15:57 -0500

    Fixed errors

--- client/strength-bros/src/components/HostGame.js
+++ client/strength-bros/src/components/HostGame.js
@@ -55,7 +55,7 @@ class HostGame extends Component {
     }
 
     const participants = this.state.participants.map(participant => (
-      <li>
+      <li key={participant.user_id}>
         {participant.username}:{" "}
         {participant.is_ready ? "Ready!" : "Not ready..."}
       </li>
@@ -67,51 +67,52 @@ class HostGame extends Component {
           <nav className="navbar navbar-light bg-light">
             <span className="nav-title">Room ID:</span>
             <span className="nav-title">{this.state.room_id} </span>
-          </nav>       
+          </nav>
         </div>
         <div className="container text-stuff center-block">
-            <div class="participants">
-            {this.state.participants.length}/
-              {this.state.max_participants}
-            </div>
-      
-            <p>Choose Game:</p>
-            <button
-              className={`btn ${
-                this.state.gameSelected === "squatRace"
-                  ? "btn-primary"
-                  : "btn-light"
-              }`}
-              name="squatRace"
-              onClick={this.onSelectGame}
-            >
-              Squat Race
-            </button>
-            <button
-              className={`btn ${
-                this.state.gameSelected === "thePlank" ? "btn-primary" : "btn-light"
-              }`}
-              name="thePlank"
-              onClick={this.onSelectGame}
-              disabled
-            >
-              The Plank
-            </button>
-            {this.state.participants.length > 0 ? (
-              <div>
-                <p>Participants:</p>
-                <ul>{participants}</ul>
-              </div>
-            ) : (
-              <p>No participants so far... go annoy your friends to join.</p>
-            )}
-            {this.state.allPlayersReady && (
-              <button className="btn btn-success" onClick={this.onStartGame}>
-                Start Game
-              </button>
-            )}
+          <div className="participants">
+            {this.state.participants.length}/{this.state.max_participants}
           </div>
+
+          <p>Choose Game:</p>
+          <button
+            className={`btn ${
+              this.state.gameSelected === "squatRace"
+                ? "btn-primary"
+                : "btn-light"
+            }`}
+            name="squatRace"
+            onClick={this.onSelectGame}
+          >
+            Squat Race
+          </button>
+          <button
+            className={`btn ${
+              this.state.gameSelected === "thePlank"
+                ? "btn-primary"
+                : "btn-light"
+            }`}
+            name="thePlank"
+            onClick={this.onSelectGame}
+            disabled
+          >
+            The Plank
+          </button>
+          {this.state.participants.length > 0 ? (
+            <div>
+              <p>Participants:</p>
+              <ul>{participants}</ul>
+            </div>
+          ) : (
+            <p>No participants so far... go annoy your friends to join.</p>
+          )}
+          {this.state.allPlayersReady && (
+            <button className="btn btn-success" onClick={this.onStartGame}>
+              Start Game
+            </button>
+          )}
         </div>
+      </div>
     );
   }
 }
--- client/strength-bros/src/components/Landing.js
+++ client/strength-bros/src/components/Landing.js
@@ -12,7 +12,7 @@ class Landing extends Component {
       username: "",
       user_id: "",
       failedUserIn: false,
-      failedCodeIn: false,
+      failedCodeIn: false
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -35,38 +35,35 @@ class Landing extends Component {
 
   onClickJoin = e => {
     e.preventDefault();
-    if (this.state.username===""){
-      this.setState({failedUserIn: true})
-
-    }else{
-      this.setState({failedUserIn: false})
+    if (this.state.username === "") {
+      this.setState({ failedUserIn: true });
+    } else {
+      this.setState({ failedUserIn: false });
     }
-    if (this.state.value===""){
-      this.setState({failedCodeIn:true})
-
+    if (this.state.value === "") {
+      this.setState({ failedCodeIn: true });
+    } else {
+      this.setState({ failedCodeIn: false });
     }
-    else{
-      this.setState({failedCodeIn:false})
+    if (!this.state.failedCodeIn || !this.state.failedUserIn) {
+      const socket = io.connect("http://138.197.166.233:6969");
+
+      socket.emit("join_room", {
+        room_id: this.state.value,
+        username: this.state.username
+      });
+
+      socket.once("room_connection_successful", data => {
+        console.log(data);
+        this.setState({ isJoin: true, user_id: data.user_id });
+      });
     }
-    if (!this.state.failedCodeIn || !this.state.failedUserIn){
-    const socket = io.connect("http://138.197.166.233:6969"); 
-
-    socket.emit("join_room", { room_id: this.state.value, username: this.state.username });
-
-    socket.once("room_connection_successful", data => {
-      console.log(data);
-      this.setState({ isJoin: true, user_id: data.user_id });
-    });
-  }
- };
+  };
 
   render() {
     if (this.state.isJoin) {
       return (
-        <AthleteView
-          username={this.state.user_id}
-          room_id={this.state.value}
-        />
+        <AthleteView user_id={this.state.user_id} room_id={this.state.value} />
       );
     }
     if (this.state.clickedCreate) {
@@ -77,44 +74,50 @@ class Landing extends Component {
         <div className="gridChild container">
           <nav className="navbar navbar-light bg-light">
             <span className="nav-title">Strength Bros.</span>
-          </nav>       
+          </nav>
         </div>
         <div className="gridChild container center-block">
-            <form className="form container" onSubmit={this.onClickJoin}>
-              <div className="form-group container">
+          <form className="form container" onSubmit={this.onClickJoin}>
+            <div className="form-group container">
               <input
-                  className="form-control mr-sm-1"
-                  type="search"
-                  placeholder="Username"
-                  aria-label="Search"
-                  name ="username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                />
-                <input
-                  className="form-control mr-sm-1"
-                  type="search"
-                  placeholder="Join Code"
-                  aria-label="Search"
-                  name ="value"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                />
+                className="form-control mr-sm-1"
+                type="search"
+                placeholder="Username"
+                aria-label="Search"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <input
+                className="form-control mr-sm-1"
+                type="search"
+                placeholder="Join Code"
+                aria-label="Search"
+                name="value"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
             </div>
-              <div className="form-group">
-                <button className="btn btn btn-primary" type="submit">
+            <div className="form-group">
+              <button className="btn btn btn-primary" type="submit">
                 Join
               </button>
             </div>
-            </form>
-          <div className="error">{this.state.failedCodeIn ? "Code cannot be blank": ""}</div>
-          <div className="error">{this.state.failedUserIn ? "Username cannot be blank": ""}</div>
+          </form>
+          <div className="error">
+            {this.state.failedCodeIn ? "Code cannot be blank" : ""}
           </div>
-        <div  className="gridChild">
-            <form onSubmit={this.onClickCreate}>
-              <button className="btn btn-large btn btn-outline-dark">Create Game</button>
-            </form>
+          <div className="error">
+            {this.state.failedUserIn ? "Username cannot be blank" : ""}
           </div>
+        </div>
+        <div className="gridChild">
+          <form onSubmit={this.onClickCreate}>
+            <button className="btn btn-large btn btn-outline-dark">
+              Create Game
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
